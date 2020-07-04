4c874fe89177088b1aee325887d492129cac9417
Nisha Thapa <nishathapa4727@gmail.com>
2020-07-04 22:04:25 +0530

    Updated Book-list app

--- app.js
+++ app.js
@@ -61,6 +61,18 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form); 
+        
+        // vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields(){
             document.querySelector('#title').value = '';
             document.querySelector('#author').value = '';
@@ -90,15 +102,24 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    //Instantiate book
 
-    const book = new Book(title, author, isbn);
+    //Validate
+    if(title === '' || author === '' || isbn === ''){
+        UI.showAlert('Please fill in all the fields', 'success');
+    } else {
+        //Instantiate book
 
-    //Add book to UI
-    UI.addBookToList(book);
+        const book = new Book(title, author, isbn);
 
-    // Clear fields
-    UI.clearFields();
+        //Add book to UI
+        UI.addBookToList(book);
+
+        //Show success message
+        UI.showAlert('Book Added', 'success');
+
+        // Clear fields
+        UI.clearFields();
+    }    
 });
 
 
@@ -106,4 +127,7 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target)
+
+     //Show success message
+     UI.showAlert('Book Removed', 'success');
 });
